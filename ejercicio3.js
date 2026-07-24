0dea19eba90ab3b4f412d4b0394eb919112eb77e
mboada <isaac270307@gmail.com>
2026-07-24 18:58:52 -0500

    ejercicio3

---
+++ ejercicio3.js
@@ -0,0 +1,9 @@
+let precioProducto = 500;
+let descuento = 0.15; // 15%
+
+let dineroAhorrado = precioProducto * descuento;
+let precioFinal = precioProducto - dineroAhorrado;
+
+console.log("Precio original: $" + precioProducto);
+console.log("Dinero ahorrado: $" + dineroAhorrado);
+console.log("Precio final con descuento: $" + precioFinal);
