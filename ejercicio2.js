9e70b2f76bb1a365f4bd7cae8fba4d02325f5b8a
mboada <isaac270307@gmail.com>
2026-07-24 18:53:48 -0500

    ejercicio2

---
+++ ejercicio2.js
@@ -0,0 +1,18 @@
+
+let nombreProducto1 = "Laptop HP";
+let precioProducto1 = 650.99;
+let disponibleProducto1 = true;
+
+let nombreProducto2 = "Mouse inalámbrico";
+let precioProducto2 = 15.50;
+let disponibleProducto2 = false;
+
+console.log("--- Producto 1 ---");
+console.log("Nombre: " + nombreProducto1);
+console.log("Precio: $" + precioProducto1);
+console.log("Disponible en stock: " + disponibleProducto1);
+
+console.log("--- Producto 2 ---");
+console.log("Nombre: " + nombreProducto2);
+console.log("Precio: $" + precioProducto2);
+console.log("Disponible en stock: " + disponibleProducto2);
