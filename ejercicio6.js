9d1ce9d0095c9112a7fe60cd8e5ac91ca196a8f7
mboada <isaac270307@gmail.com>
2026-07-24 19:11:44 -0500

    ejercicio6.js pruebas

--- ejercicio6.js
+++ ejercicio6.js
@@ -7,8 +7,8 @@ function obtenerMayor(num1, num2) {
     }
 }
 
-let resultado1 = obtenerMayor(60, 25);
+let resultado1 = obtenerMayor(60, 920);
 let resultado2 = obtenerMayor(48, 12);
 
-console.log("El mayor entre 60 y 25 es: " + resultado1);
+console.log("El mayor entre 60 y 920 es: " + resultado1);
 console.log("El mayor entre 48 y 12 es: " + resultado2);
