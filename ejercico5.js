590d30838910140df750412806d6725e5d863beb
mboada <isaac270307@gmail.com>
2026-07-24 19:05:23 -0500

    ejercicio5.js

---
+++ ejercico5.js
@@ -0,0 +1,12 @@
+let sumaTotal = 0;
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " es par");
+    } else {
+        console.log(i + " es impar");
+    }
+    sumaTotal += i;
+}
+
+console.log("La suma total de los números del 1 al 20 es: " + sumaTotal);
